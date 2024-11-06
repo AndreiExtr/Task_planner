5f07acb52776263999085f095d3a71456c8734c4
Андрей кропинов <andreikropinov17121988@gmail.com>
2024-11-06 22:28:55 +0300

    edit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,13 +13,22 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  build: {
-    rollupOptions: {
-      output: {
-        // настройки для вывода файлов
-        chunkFileNames: 'style.css',
-        entryFileNames: 'style.css',
-      }
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Task_planner/'
+    : '/',
+  css: {
+    extract: {
+      filename: 'style.css',
+      chunkFilename: 'style.css'
     }
+  },
+  configureWebpack: {
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'style.css',
+        chunkFilename: 'style.css'
+      })
+    ]
   }
 })
